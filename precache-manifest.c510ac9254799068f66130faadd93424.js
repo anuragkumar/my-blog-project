ca8eab83af7819d75dee763da2504ea1dd03abd3
Anurag Kumar <kumar.anur@husky.neu.edu>
2020-09-14 16:29:50 -0400

    Updates

---
+++ precache-manifest.c510ac9254799068f66130faadd93424.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d440fbf30f4f10b6cc0fb11ddd6371d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3a481ef7ec62b58b385",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "5c40c0e4a34d9e591486",
+    "url": "/static/css/main.8f243adf.chunk.css"
+  },
+  {
+    "revision": "f3a481ef7ec62b58b385",
+    "url": "/static/js/2.1db9046a.chunk.js"
+  },
+  {
+    "revision": "2f71fbc01fec0c62d6e32185bc021541",
+    "url": "/static/js/2.1db9046a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c40c0e4a34d9e591486",
+    "url": "/static/js/main.ce08a612.chunk.js"
+  },
+  {
+    "revision": "770344394e9f4453d0a9",
+    "url": "/static/js/runtime-main.3a907230.js"
+  },
+  {
+    "revision": "21572c33193162976746cccac79c8797",
+    "url": "/static/media/background-code.21572c33.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f29b7d7626354b367c3a68162f5b8cc7",
+    "url": "/static/media/pp.f29b7d76.jpg"
+  }
+]);
